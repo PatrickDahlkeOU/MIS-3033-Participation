c7897cd285fd25a8d8963349eabbfe3192407e0c
Patrick Dahlke <patrickdahlke@ou.edu>
2021-02-02 14:28:52 -0600

    Attempted first WPF application

--- In Class Example 2-2/WPF First Application/MainWindow.xaml.cs
+++ In Class Example 2-2/WPF First Application/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +26,22 @@ namespace WPF_First_Application
         public MainWindow()
         {
             InitializeComponent();
+
+            lblhidden.Content = string.Empty;
+            txtTest1.Clear();
+
+
+        }
+
+        private void Submit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //MessageBox.Show("You clicked me!");
+            MessageBox.Show("Thanks for clicking me!", "Clicked", MessageBoxButton.YesNo);
+            lblhidden.Content = "Hello World";
+
+            string answer = txtTest1.Text;
+            double result = Convert.ToDouble(answer);
+
         }
     }
 }
