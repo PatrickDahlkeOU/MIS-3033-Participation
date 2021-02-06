90058b6a014703873477b4df700ecfae9c644aeb
Patrick Dahlke <patrickdahlke@ou.edu>
2021-02-05 19:09:53 -0600

    Final Product

--- (P) First WPF Application/WPFApplication/MainWindow.xaml.cs
+++ (P) First WPF Application/WPFApplication/MainWindow.xaml.cs
@@ -27,10 +27,27 @@ namespace WPFApplication
             txtBirthdate.Text = string.Empty;
         }
 
+        private void btnEnter(object sender, MouseEventArgs e)
+        {
+            GrdColor.Background = Brushes.Orange;
+        }
 
+        private void btnLeave(object sender, MouseEventArgs e)
+        {
+            GrdColor.Background = (Brush)new BrushConverter().ConvertFrom("#FF0F4C8E");
+        }
 
+        private void btnCalculateClick(object sender, RoutedEventArgs e)
+        {
+            string answerN = txtNames.Text;
 
+            string answerD = txtBirthdate.Text;
+            DateTime NumberBirth = Convert.ToDateTime(answerD);
+            DateTime Today = DateTime.Today;
 
+            TimeSpan result = Today - NumberBirth;
 
+            MessageBox.Show("Thanks " + answerN + ", " + "you are " + ((result.TotalDays)/365) + " years old!");
+        }
     }
 }
