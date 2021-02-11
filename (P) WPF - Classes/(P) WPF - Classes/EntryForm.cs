0569f69185df86a1d6d8770749ae7931df99ee5d
Patrick Dahlke <patrickdahlke@ou.edu>
2021-02-11 14:29:22 -0600

    In Class Time Set Up

---
+++ (P) WPF - Classes/(P) WPF - Classes/EntryForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P__WPF___Classes
+{
+    public class EntryForm
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int Zipcode { get; set; }
+
+        public EntryForm ()
+        {
+            Name = String.Empty ;
+
+            Address = String.Empty ;
+
+            Zipcode = 0;
+
+        }
+
+        public EntryForm ( string name, string address, int zipCode )
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "Placement Words";
+        }
+
+
+
+
+
+
+    }
+}
