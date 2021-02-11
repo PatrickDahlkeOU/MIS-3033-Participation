96171ee50835474dbcdcc83f2a71e58860152968
Patrick Dahlke <patrickdahlke@ou.edu>
2021-02-11 13:16:29 -0600

    Attempt 1

---
+++ Wpf Class 2-9/Wpf Class 2-9/Pet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wpf_Class_2_9
+{
+    class Pet
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string PictureURL { get; set; }
+
+        public Pet()
+        {
+            Name = string.Empty;
+            Breed = string.Empty;
+            PictureURL = string.Empty;
+        }
+
+
+
+    }
+}
